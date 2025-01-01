a88e0783e4110510ca35f51652bb389f28c526e1
Vladimir Tchuiev <rmcf126@gmail.com>
2025-01-01 22:51:25 +0200

    Added footer + header

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p className="text-6xl font-bold">Hi everyone! Daisy Feeder 2</p>
+      <p className="text-xl">Marketing landing page</p>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Edu_AU_VIC_WA_NT_Hand } from "next/font/google";
+import { Edu_AU_VIC_WA_NT_Hand, Lilita_One } from "next/font/google";
 import "./globals.css";
 
-const font = Edu_AU_VIC_WA_NT_Hand({ subsets: ["latin"] });
+const font = Lilita_One({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Daisy Feeder 2",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={`${font.className} antialiased`}>{children}</body>
     </html>
   );
 }
