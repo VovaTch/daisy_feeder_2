3d1a8bbfe95ec29de31604645b70498e11456705
Vladimir Tchuiev <rmcf126@gmail.com>
2025-01-10 20:56:53 +0200

    Dealt with the small margin post footer

--- app/main/(daily)/footer.tsx
+++ app/main/(daily)/footer.tsx
@@ -2,7 +2,7 @@ import AddFoodDialog from "@/components/dialogs/add-food";
 
 const DailyFooter = () => {
   return (
-    <footer className="absolute left-0 bottom-0 items-center justify-center w-full h-20">
+    <footer className="absolute left-0 bottom-0 items-center justify-center w-full h-18">
       <div className="flex flex-col items-center justify-center w-full">
         <AddFoodDialog />
       </div>
