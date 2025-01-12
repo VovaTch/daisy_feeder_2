8b6166670b59a919abf7699a095c0ad212396a51
Vladimir Tchuiev <rmcf126@gmail.com>
2025-01-12 22:14:04 +0200

    The total-per-day bar chart is ready outside the point scaling and testing

--- app/main/statistics/[period]/page.tsx
+++ app/main/statistics/[period]/page.tsx
@@ -3,6 +3,7 @@ import DaisyChartContainer from "@/components/charts/chart-container";
 import { ChartBarExample } from "@/components/charts/bar-example";
 import { FeedingItem } from "@/components/types/food-item";
 import { ChartLineExample } from "@/components/charts/line-example";
+import DailyFoodLineChart from "@/components/charts/total-per-day";
 
 type StatisticsPageProps = {
   params: {
@@ -12,10 +13,10 @@ type StatisticsPageProps = {
 
 const StatisticsPage = async ({ params }: StatisticsPageProps) => {
   const { period } = await params;
-  const FeedingData: FeedingItem[] = DummyData; // TODO: set real data when ready
+  const feedingData: FeedingItem[] = DummyData; // TODO: set real data when ready
   return (
     <div
-      className="flex flex-wrap flex-row absolute w-full lg:h-[calc(100vh-80px)] h-[calc(100vh-130px)] overflow-y-auto content-start
+      className="flex flex-wrap flex-row absolute w-full lg:h-[calc(100vh-160px)] h-[calc(100vh-210px)] overflow-y-auto content-start
     items-start justify-start"
     >
       <DaisyChartContainer>
@@ -24,7 +25,9 @@ const StatisticsPage = async ({ params }: StatisticsPageProps) => {
       <DaisyChartContainer>
         <ChartLineExample />
       </DaisyChartContainer>
-      <div className="lg:h-[28vw] lg:w-[28vw] h-[100vw] w-[100vw] m-5 bg-white rounded-sm"></div>
+      <DaisyChartContainer>
+        <DailyFoodLineChart feedingData={feedingData} dayRange={period} />
+      </DaisyChartContainer>
       <div className="lg:h-[28vw] lg:w-[28vw] h-[100vw] w-[100vw] m-5 bg-white rounded-sm"></div>
     </div>
   );
--- components/charts/total-per-day.tsx
+++ components/charts/total-per-day.tsx
@@ -1,6 +1,23 @@
+"use client";
+
+import { useState } from "react";
 import { PerBatchSumFood } from "../custom/overall-food-sum";
 import { FeedingItem } from "../types/food-item";
-import { ChartConfig } from "../ui/chart";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "../ui/chart";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 const chartConfig = {
   total: {
@@ -17,17 +34,31 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
+const dayRangeMap = {
+  "007": 7,
+  "030": 30,
+  "090": 90,
+  "365": 365,
+  "999": Infinity,
+} as const;
+
 type DailyFoodLineChartProps = {
   feedingData: FeedingItem[];
+  dayRange: "007" | "030" | "090" | "365" | "999";
 };
 
-const DailyFoodLineChart = ({ feedingData }: DailyFoodLineChartProps) => {
+const DailyFoodLineChart = ({
+  feedingData,
+  dayRange,
+}: DailyFoodLineChartProps) => {
   const dates = feedingData.map((item) =>
     item.datetime.toISOString().slice(0, 10)
   );
   const uniqueDates = [...new Set(dates)];
+  const [activeChart, setActiveChart] =
+    useState<keyof typeof chartConfig>("total");
 
-  const chartData = uniqueDates.map((date) => {
+  const chartDataAll = uniqueDates.map((date) => {
     const totalDry = PerBatchSumFood({ feedingData, foodType: "dry", date });
     const totalWet = PerBatchSumFood({ feedingData, foodType: "wet", date });
 
@@ -39,7 +70,101 @@ const DailyFoodLineChart = ({ feedingData }: DailyFoodLineChartProps) => {
     };
   });
 
-  // TODO: continue
+  const chartData = chartDataAll.filter((item) => {
+    const itemDate = new Date(item.date);
+    const itemStartDate = new Date();
+    itemStartDate.setDate(itemStartDate.getDate() - dayRangeMap[dayRange]);
+    return itemDate >= itemStartDate && itemDate <= new Date();
+  });
+
+  const total = {
+    total: chartData.reduce((acc, curr) => acc + curr.total, 0),
+    totalDry: chartData.reduce((acc, curr) => acc + curr.totalDry, 0),
+    totalWet: chartData.reduce((acc, curr) => acc + curr.totalWet, 0),
+  };
+
+  return (
+    <Card>
+      <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
+        <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
+          <CardTitle className="flex flex-row">Feeding Chart</CardTitle>
+          <CardDescription>
+            Total food
+            {dayRange === "999"
+              ? ""
+              : ` for the last ${dayRangeMap[dayRange]} days`}
+          </CardDescription>
+        </div>
+        <div className="flex justify-evenly">
+          {["total", "totalDry", "totalWet"].map((key) => {
+            const chart = key as keyof typeof chartConfig;
+            return (
+              <Button
+                variant={activeChart === chart ? "sidebarOutline" : "sidebar"}
+                className="text-xs h-full w-full hover:scale-110 transition flex flex-col"
+                key={chart}
+                data-active={activeChart === chart}
+                onClick={() => setActiveChart(chart)}
+              >
+                <h4>{chartConfig[chart].label}</h4>
+                <h1 className="text-slate-700 text-lg">{total[chart]}</h1>
+              </Button>
+            );
+          })}
+        </div>
+      </CardHeader>
+      <CardContent className="px-2 sm:p-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{ left: 12, right: 12 }}
+          >
+            <CartesianGrid vertical={true} horizontal={true} />
+            <XAxis
+              dataKey={"date"}
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value);
+                return date.toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                });
+              }}
+            />
+            <ChartTooltip
+              content={
+                <ChartTooltipContent
+                  className="w-[150px]"
+                  nameKey="amount"
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    });
+                  }}
+                />
+              }
+            />
+            <Line
+              dataKey={activeChart}
+              type="monotone"
+              stroke={`var(--color-${activeChart})`}
+              strokeWidth={2}
+              dot={true}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default DailyFoodLineChart;
